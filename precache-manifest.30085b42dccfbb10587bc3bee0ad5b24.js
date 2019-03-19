873df3c6deea9e51a9e862955dfa50a2b6f78b70
KexinLu <kexin.lu@beanworks.com>
2019-03-19 00:56:01 -0700

    Updates

---
+++ precache-manifest.30085b42dccfbb10587bc3bee0ad5b24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baae8fd6c53c9e8932e9",
+    "url": "/virgil-ui/static/js/main.970aae37.chunk.js"
+  },
+  {
+    "revision": "d747dc811101e56f40c5",
+    "url": "/virgil-ui/static/js/runtime~main.e6fc7484.js"
+  },
+  {
+    "revision": "894527dbe135d03a1c95",
+    "url": "/virgil-ui/static/css/2.0096fdcc.chunk.css"
+  },
+  {
+    "revision": "894527dbe135d03a1c95",
+    "url": "/virgil-ui/static/js/2.36e46c87.chunk.js"
+  },
+  {
+    "revision": "8e7b851e44b14c5ce9d2c13b4a2b1c54",
+    "url": "/virgil-ui/index.html"
+  }
+];
